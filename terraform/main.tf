e02ab47eb0ce31c53d95391907b4dec30f49f960
DoppiaF <francesco.furini@edu.unife.it>
2024-09-30 12:57:31 +0200

    aggiunto sandbox-aft account

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,10 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
+    AccountEmail              = "francesco.poc.aft+aft-sandbox@gmail.com"
     AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
+    ManagedOrganizationalUnit = "Management"
+    SSOUserEmail              = "francesco.poc.aft+aft-sandbox@gmail.com"
     SSOUserFirstName          = "Sandbox"
     SSOUserLastName           = "AFT"
   }
